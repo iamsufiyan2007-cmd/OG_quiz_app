f041bff5440ecb4725e16b02831299814fd133d5
iamsufiyan2007-cmd <iamsufiyan2007@gmail.com>
2025-12-12 09:33:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from google import genai
 import json
 
-client = genai.Client(api_key=st.secrets('GOOGLE_API_KEY'))
+client = genai.Client(api_key=st.secrets['GOOGLE_API_KEY'])
 
 st.title("Goated Quiz")
 
